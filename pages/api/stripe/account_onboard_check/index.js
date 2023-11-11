fea811487207b63e80b818e9e61eda57993bd241
Juliano JC <julianocordeir@gmail.com>
2023-11-11 17:35:35 -0300

    update

--- pages/api/stripe/account_onboard_check/index.js
+++ pages/api/stripe/account_onboard_check/index.js
@@ -6,12 +6,16 @@ const host = process.env.NEXT_PUBLIC_HOST
 const stripeOnboardingCheck = async (req, res) => {
     if (method === "GET") {
         try{
-            // verify boolean > details_submitted < is true
-            const response = await stripe.accounts.retrieve( req.body.accountID );
 
-            return res.status(200).json({
-                isActive: response.details_submitted
-            })
+            console.log(req.query.accountID)
+            // verify boolean > details_submitted < is true
+            const account = await stripe.accounts.retrieve( req.query.accountID );
+
+            return res.status(200).json({ account })
+            
+            // return res.status(200).json({
+            //     isActive: response.details_submitted
+            // })
         }
         catch(e){
             return res.status(400).send({
@@ -19,6 +23,10 @@ const stripeOnboardingCheck = async (req, res) => {
             });
         }
        
+    }else{
+        return res.status(400).send({
+            error:{ message: e?.message }
+        });
     }
 }
 
