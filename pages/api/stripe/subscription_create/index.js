65fef824b483140eb3bdbacb86638ff5c3e6fbc2
Juliano JC <julianocordeir@gmail.com>
2023-11-15 21:57:58 -0300

    update

--- pages/api/stripe/subscription_create/index.js
+++ pages/api/stripe/subscription_create/index.js
@@ -6,7 +6,7 @@ const stripeSubscriptionCreate = async (req, res) => {
   
     const { method } = req
     const _customerID = req.body.customerID;
-    const _connectedAccountID = req.body.accountID;
+    const _connectedAccountID = req.body.accConnectID;
     const _priceID= req.body.priceID;
   
     if (method === "POST") {
