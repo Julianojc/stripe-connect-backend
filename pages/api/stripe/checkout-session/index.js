e99aa0a7169b857ed3ce14579eb2e96a15a450a1
Juliano JC <julianocordeir@gmail.com>
2023-11-16 16:55:55 -0300

    update

--- pages/api/stripe/checkout-session/index.js
+++ pages/api/stripe/checkout-session/index.js
@@ -1,49 +1,53 @@
 const stripe = require("stripe")(process.env.STRIPE_API_SECRET)
 const host = process.env.NEXT_PUBLIC_HOST
-const appFee = process.env.STRIPE_APP_FEE
 
 /**
  * Generates a checkout session based on the Connected Account Id and other
  * data provided. Handles cases for both the flutter app and the web.
  */
 const checkoutSession = async (req, res) => {
-  const {
-    account_id: accountId,
-    amount,
-    title,
-    currency = "eur",
-    quantity,
-    mobile,
-  } = req.query
-  const session = await stripe.checkout.sessions.create({
-    payment_method_types: ["card"],
-    line_items: [
-      {
-        name: title,
-        /**
-         * Multiplying by 100 because otherwise for example,
-         * 149 becomes 1.49 on Stripe
-         */
-        amount: Math.round(amount) * 100,
-        currency,
-        quantity,
-      },
-    ],
-    payment_intent_data: {
-      /**
-       * Multiplying by 100 because otherwise for example,
-       * 149 becomes 1.49 on Stripe
-       */
-      application_fee_amount: appFee * 100,
-      transfer_data: {
-        destination: accountId,
-      },
-    },
-    mode: "payment",
-    success_url: `${host}/pay-out${mobile ? "-mobile" : ""}?result=success`,
-    cancel_url: `${host}/pay-out${mobile ? "-mobile" : ""}?result=failure`,
-  })
-  res.status(200).json({ session })
+  
+  const { accountConnectId, customerId, priceId, currency = "BRL", mobile } = req.query
+
+  if (req.method === 'POST') {
+        
+    try {
+      // Create Checkout Sessions from body params.
+      const params = {
+        customer: customerId,
+        mode: 'subscription',
+        subscription_data: {
+          application_fee_percent: process.env.STRIPE_APP_FEE, //porcentagem de repasse para plataforma
+          transfer_data: {
+            destination: `${accountConnectId}`, //ID DO CRIADOR
+            //amount_percent: 95
+          }, 
+        },
+        line_items: [
+          {
+            price: `${priceId}`,
+            quantity: 1,
+          },
+        ],
+        //success_url: `${req.headers.origin}/result?session_id={CHECKOUT_SESSION_ID}`,
+        //cancel_url: `${req.headers.origin}/donate-with-checkout`,
+
+        success_url: `${CALLBACK_APP_URL }/feed`,
+        cancel_url: `${CALLBACK_APP_URL }/settings`,
+      }
+      const checkoutSession = await stripe.checkout.sessions.create(params)
+      res.status(200).json({checkoutSession}) //return
+
+    } catch (err) {
+      const errorMessage =
+        err instanceof Error ? err.message : 'Internal server error'
+      res.status(500).json({ statusCode: 500, message: errorMessage })
+    }
+  } else {
+    //ERROR
+    res.setHeader('Allow', 'POST')
+    res.status(405).end('Method Not Allowed')
+  }
 }
 
 export default checkoutSession
