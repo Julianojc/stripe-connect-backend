83719852eb64bde1a05dce52e3358c667a3fb63e
Juliano JC <julianocordeir@gmail.com>
2023-11-15 22:25:35 -0300

    update

--- pages/api/stripe/subscription_create/index.js
+++ pages/api/stripe/subscription_create/index.js
@@ -1,6 +1,6 @@
 
 
-const stripe = require("stripe")( process.env.STRIPE_API_SECRET )
+const stripe = require("stripe")( process.env.NEXT_PUBLIC_STRIPE_API_PUBLIC )
 
 const stripeSubscriptionCreate = async (req, res) => {
   
