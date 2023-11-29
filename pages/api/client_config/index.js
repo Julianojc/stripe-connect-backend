5602e81be62545adc1f333c731c0fbb6d0c7cb69
Juliano JC <julianocordeir@gmail.com>
2023-11-29 19:44:24 -0300

    update

--- pages/api/client_config/index.js
+++ pages/api/client_config/index.js
@@ -1,4 +1,8 @@
-import { ApolloClient, InMemoryCache, HttpLink } from '@apollo/client';
+import { ApolloClient, InMemoryCache, HttpLink, split } from '@apollo/client';
+import { getMainDefinition } from '@apollo/client/utilities';
+import { WebSocketLink } from "@apollo/client/link/ws";
+import { createClient } from 'graphql-ws';
+import { GraphQLWsLink } from '@apollo/client/link/subscriptions';
 
 //APOLLO CLIENT CONFIG
 
@@ -8,20 +12,17 @@ const httpsLink = new HttpLink({
       'x-hasura-admin-secret': process.env.HASURA_DB_ADMIN_SECRET
     }
   });
-  
-  const wssLink = new WebSocketLink({
-    uri: process.env.HASURA_ENDPOINT,
-    options: {
-      reconnect: true,
-      connectionParams: {
-        headers: {
-          'x-hasura-admin-secret': process.env.HASURA_DB_ADMIN_SECRET
-        }
+
+  const wssLink = new GraphQLWsLink(createClient({
+    url: process.env.HASURA_ENDPOINT,
+    connectionParams: {
+      headers: {
+        'x-hasura-admin-secret': process.env.HASURA_DB_ADMIN_SECRET
       }
     }
-  });
-  
-  const link = split(
+  }));
+    
+  const splitlink = split(
     ({ query }) => {
       const definition = getMainDefinition(query);
       return (
@@ -35,8 +36,9 @@ const httpsLink = new HttpLink({
   
   const createApolloClient = () => {
     return new ApolloClient({
+      link: splitlink,
       cache: new InMemoryCache(),
-      link
+      
     });
   };
   
