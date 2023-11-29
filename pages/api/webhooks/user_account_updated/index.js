2ef322012025e3d4b20ce1e15aa260f7eec7f15a
Juliano JC <julianocordeir@gmail.com>
2023-11-29 19:54:04 -0300

    update

--- pages/api/webhooks/user_account_updated/index.js
+++ pages/api/webhooks/user_account_updated/index.js
@@ -1,6 +1,6 @@
 
 import { client } from "../../client_config/index.js"
-import { gql, useMutation } from '@apollo/client';
+import { gql } from '@apollo/client';
 const stripe = require('stripe')( process.env.STRIPE_API_SECRET );
 
 
@@ -60,26 +60,35 @@ export default async function handler(req, res){
       console.log(`user atualizado > ${accountUpdated}` ) //data
       
       if(accountUpdated.charges_enabled && accountUpdated.details_submitted){  
-          // SEND GRAPHQL MUTATION  
-          var obj = await client.mutate({
-            mutation: gql`
-            mutation UpdateUser($id: String!, $role: user_roles_enum, $stripe_connect_id: String!) {
-              update_user_by_pk(
-                pk_columns: {id: $id}, 
-                _set: {role: $role, stripe_connect_id: $stripe_connect_id}) 
-                {
-                  id
-                }
-            }`
-         })
+            // SEND GRAPHQL MUTATION  
+            var obj = await client.mutate({
+              mutation: gql`
+              mutation UpdateUser($id: String!, $role: user_roles_enum, $stripe_connect_id: String!) {
+                update_user_by_pk(
+                  pk_columns: {id: $id}, 
+                  _set: {role: $role, stripe_connect_id: $stripe_connect_id}) 
+                  {
+                    id
+                  }
+              }`,
+              variables:{
+                id: accountUpdated.metadata.user_id,
+                role: "CREATOR",
+                stripe_connect_id: accountUpdated.id
+              }
+          })
+          if(obj != null ){
+            // Return a 200 response to acknowledge receipt of the event
+            return res.status(200).json({
+              accountUpdated: accountUpdated.id,
+              metadata: accountUpdated.metadata,
+              userId: accountUpdated.metadata.user_id
+            })
+         }
+         else{
+          return res.status(400).send(`Webhook Error`)
+         }
       }
-
-      // Return a 200 response to acknowledge receipt of the event
-      return response.status(200).send({
-        accountUpdated: accountUpdated.id,
-        metadata: accountUpdated.metadata,
-        userId: accountUpdated.metadata.user_id
-      });
   }
   else {
     return res.status(400).send(`Webhook Error`)
