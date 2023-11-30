308805ef1772f8a6792e65b8b251035a742826b2
Juliano JC <julianocordeir@gmail.com>
2023-11-30 14:38:44 -0300

    up

--- pages/api/webhooks/user_account_updated/index.js
+++ pages/api/webhooks/user_account_updated/index.js
@@ -3,14 +3,7 @@ import { gql } from '@apollo/client';
 import { buffer } from 'micro';
 
 const stripe = require('stripe')( process.env.STRIPE_API_SECRET, { apiVersion: null } );
-const webhook_secret = process.env.STRIPE_WEBHOOK_ACCOUNTS_SECRET
-
-
-
-// import { client } from "../../client_config/index.js"
-// import { gql } from '@apollo/client';
-// const stripe = require('stripe')( process.env.STRIPE_API_SECRET );
-
+const webhook_secret = "whsec_23a81737130d622b6125d20841fd7ee9314f480048565b8e8c371564d398e6d6"//process.env.STRIPE_WEBHOOK_ACCOUNTS_SECRET
 
 export const config = {
   api: {
