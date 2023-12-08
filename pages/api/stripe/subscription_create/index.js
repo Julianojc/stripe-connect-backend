d652bd7a069ceb8c6d5e3b25a9dea9c239613b1e
Juliano JC <julianocordeir@gmail.com>
2023-12-08 16:55:27 -0300

    update

--- pages/api/stripe/subscription_create/index.js
+++ pages/api/stripe/subscription_create/index.js
@@ -7,7 +7,7 @@ export default async function stripeSubscriptionCreate (req, res) {
     const { method } = req
 
     const {
-        query: { customerId, accConnectId, priceId, userId },
+        query: { customerId, accConnectId, priceId, userCreatorId, userClientId, modalityId },
     } = req
       
     if (method === "POST") {
@@ -28,13 +28,27 @@ export default async function stripeSubscriptionCreate (req, res) {
                 expand: ['latest_invoice.payment_intent'],
                 transfer_data: {
                   destination: accConnectId, // conta connect do criador
+                  //amount_percent: 92
                 },
                 metadata:{
-                    "creator_id": userId
+                    "creator_id": userCreatorId,
+                    "client_id": userClientId,
+                    "modality_id": modalityId,
                 }
             }
                         
             const subscription = await stripe.subscriptions.create( params );
+
+            await saveSubscInDB({
+                intent_id: subscription.payment_intent.id,
+                subscription_id: subscription.id,
+                creator_id: userCreatorId,
+                client_id: userClientId,
+                modality_id: modalityId,
+                premium: true,
+                active: false,
+                status: "INCOMPLETE"
+            })
 
             // RETORNA
             res.status(200).json({ 
@@ -54,3 +68,58 @@ export default async function stripeSubscriptionCreate (req, res) {
         return;
     }
 }
+
+
+
+//////////SAVE IN HASURA
+
+async function saveSubscInDB({creator_id, client_id, subscription_id, intent_id, modality_id, status, active, premium}){
+    try{
+        
+        var data = await client.mutate({
+          mutation: gql`
+          mutation MyMutation(
+            $active: Boolean!, 
+            $premium: Boolean!, 
+            $stripe_subscription_id: String!,
+            stripe_payment_intent_id: String!, 
+            $user_client_id: String!, 
+            $user_creator_id: String!, 
+            $payment_status: subscription_status_enum!, 
+            $modality_id: uuid!
+            ){
+            insert_subscription_one(
+              object: {
+                active: $active, 
+                premium: $premium, 
+                stripe_subscription_id: $stripe_subscription_id,
+                stripe_payment_intent_id: $stripe_payment_intent_id, 
+                user_client_id: $user_client_id, 
+                user_creator_id: $user_creator_id, 
+                payment_status: $payment_status, 
+                modality_id: $modality_id
+              }){
+              id
+            }
+          }                    
+          `,
+          variables:{
+            stripe_subscription_id: subscription_id,
+            stripe_payment_intent_id: intent_id,
+            payment_status: status,
+            premium: premium,
+            active: active,
+            user_creator_id: creator_id,
+            user_client_id: client_id,
+            modality_id: modality_id
+          }
+        })
+        if(data != null ){
+          // Return a 200 response to acknowledge receipt of the event
+          return data          
+        }
+    }
+    catch(e){
+      console.log(e)
+    }
+  }
---
+++ pages/api/webhooks/subscriptions/index.js
@@ -0,0 +1,202 @@
+import { client } from "../../client_config/index.js"
+import { gql } from '@apollo/client';
+import { buffer } from 'micro';
+
+const stripe = require('stripe')( process.env.NEXT_STRIPE_API_SECRET, { apiVersion: null } );
+const webhook_secret = process.env.STRIPE_WEBHOOK_ACCOUNTS_SECRET
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+}
+
+export default async function handler(req, res){
+    
+  if (req.method !== "POST") {
+    return
+  }
+  
+  const requestBuffer = await buffer(req)
+  const signature = req.headers['stripe-signature'];
+
+  let event
+
+  // Verify webhook signature and extract the event.
+  // See https://stripe.com/docs/webhooks/signatures for more information.
+  try {
+    event = stripe.webhooks.constructEvent(
+      requestBuffer.toString(), 
+      signature, 
+      webhook_secret
+    )
+
+    // Extract the object from the event.
+    const dataObject = event.data.object;
+    //const metadata = dataObject['metadata']
+
+    // Handle the event
+    // Review important events for Billing webhooks
+    // https://stripe.com/docs/billing/webhooks
+    // Remove comment to see the various objects sent for this sample
+    switch (event.type) {
+        
+        case 'invoice.payment_succeeded':
+          if(dataObject['billing_reason'] == 'subscription_create') {
+            
+            // A assinatura é ativada automaticamente após o pagamento bem-sucedido
+            // Define a forma de pagamento utilizada para pagar a primeira fatura
+            // como método de pagamento padrão para essa assinatura
+            const subscription_id = dataObject['subscription']
+            const payment_intent_id = dataObject['payment_intent']
+  
+            // Recupera a intenção de pagamento usada para pagar a assinatura
+            const payment_intent = await stripe.paymentIntents.retrieve(payment_intent_id);
+  
+            try {
+              const subscription = await stripe.subscriptions.update(
+                subscription_id,
+                {
+                  default_payment_method: payment_intent.payment_method,
+                },
+              );
+              
+              await updateDATABASE({subscription_id: subscription_id, status: 'ACTIVE', active: true}) // UPDATE HASURA DATABASE
+
+              console.log("Default payment method set for subscription:" + payment_intent.payment_method);
+            } catch (err) {
+              console.log(err);
+              console.log(`⚠️  Falied to update the default payment method for subscription: ${subscription_id}`);
+            }
+          };
+  
+          break;
+          
+        case 'invoice.payment_failed':
+           // Se o pagamento falhar ou o cliente não tiver um método de pagamento válido,
+           // um evento fatura.payment_failed é enviado, a assinatura se torna past_due.
+           // Use este webhook para notificar seu usuário de que o pagamento dele foi
+           // falhou e para recuperar os detalhes do novo cartão.
+           const subscription_id = dataObject['subscription']
+           const payment_intent_id = dataObject['payment_intent']
+           await updateDATABASE({subscription_id: subscription_id, status: 'PAYMENT_FAILED', active: false}) // UPDATE HASURA DATABASE
+          break;
+        
+        case 'invoice.finalized':
+           // Se você deseja enviar faturas manualmente para seus clientes
+           // ou armazene-os localmente para referência para evitar atingir os limites de taxa do Stripe.
+           await saveInvoiceInDATABASE()
+          break;
+        
+        case 'customer.subscription.deleted':
+          if (event.request != null) {
+            // trata de uma assinatura cancelada por sua solicitação de cima.
+            const subscription_id = dataObject['subscription']
+            const payment_intent_id = dataObject['payment_intent']
+            await updateDATABASE({subscription_id: subscription_id, status: 'CANCELLED', active: false}) // UPDATE HASURA DATABASE
+          } else {
+            // trata a assinatura cancelada automaticamente com base
+            // nas configurações da sua assinatura.
+            const subscription_id = dataObject['subscription']
+            const payment_intent_id = dataObject['payment_intent']
+            await updateDATABASE({subscription_id: subscription_id, status: 'CANCELLED', active: false}) // UPDATE HASURA DATABASE
+          }
+          break;
+        
+        case 'customer.subscription.trial_will_end':
+         //Envia uma notificação ao seu usuário de que o periodo de teste "TRIAL" será concluído
+          break;
+        default:
+        // Unexpected event type
+      }
+      res.sendStatus(200);
+    }
+    catch(e){
+        return res.status(400).send(`Webhook Error: ${err.message}`)
+    }
+}
+
+
+
+
+//////////SAVE IN HASURA
+
+async function updateDATABASE({subscription_id, status, active}){
+  try{
+
+      const _mutation = gql`
+      mutation updateSubs(
+        $subscription_id: String!,
+        $payment_status: subscription_status_enum!,
+        $active: Boolean!
+        ){
+        update_subscription(
+          where: {stripe_subscription_id: {_eq: $subscription_id}}, 
+          _set: {
+            active: $active, 
+            payment_status: $payment_status
+          }){
+          affected_rows
+        }
+      }       
+      `;
+
+      var data = await client.mutate({
+        mutation: _mutation,
+        variables:{
+          subscription_id: subscription_id,
+          payment_status: status,
+          active: active,
+        }
+      })
+      if(data != null ){
+        // Return a 200 response to acknowledge receipt of the event
+        console.log(data)
+      }
+  }
+  catch(e){
+    console.log(e)
+  }
+}
+
+//// SAVE INVOICE IN HASURA DB
+
+async function saveInvoiceInDATABASE({subscription_id, status, active}){
+  try{  
+
+      const _mutation = gql`
+      mutation updateSubs(
+        $ubscription_id: String!,
+        $payment_status: subscription_status_enum!,
+        $active: Boolean!
+        ){
+        update_subscription(
+          where: {stripe_subscription_id: {_eq: $ubscription_id}}, 
+          _set: {
+            active: $active, 
+            payment_status: $payment_status
+          }){
+          affected_rows
+        }
+      }       
+      `;      
+      var data = await client.mutate({
+        mutation: _mutation,
+        variables:{
+          subscription_id: subscription_id,
+          payment_status: status,
+          active: active,
+        }
+      })
+      if(data != null ){
+        // Return a 200 response to acknowledge receipt of the event
+        console.log(data)
+      }
+  }
+  catch(e){
+    console.log(e)
+  }
+}
+
+
+  
