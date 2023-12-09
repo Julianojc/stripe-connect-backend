cd5b09de14bbe308e02a48767694fc95619ce894
Juliano JC <julianocordeir@gmail.com>
2023-12-09 10:46:16 -0300

    update

--- pages/api/stripe/customer_create/index.js
+++ pages/api/stripe/customer_create/index.js
@@ -1,19 +1,30 @@
 
 const stripe = require("stripe")( process.env.NEXT_STRIPE_API_SECRET ) //STRIPE SECRET
 
-const stripeCustomerAccount = async (req, res) => {
+export default async function stripeCustomerAccount(req, res){
   
     const { method } = req
+
+    const {
+        query: { userID, email, name },
+    } = req
   
     if (method === "POST") {
         try{
             const customer = await stripe.customers.create({
-                email: req.body.email,
-                description: `user_${req.body.userID}`,
+                name: name,
+                email: email,
+                description: `user_${userID}`,
                 metadata:{
-                    "user_id": req.body.userID
+                    "user_id": userID
                 }
             });
+
+            updateStripeInfoDATABASE({
+                user_id: userID,
+                customer_id: customer.id,
+                email: email
+            })
 
             res.status(200).json({ 
                 success: true, 
@@ -28,4 +39,39 @@ const stripeCustomerAccount = async (req, res) => {
     }
 }
 
-export default stripeCustomerAccount
+
+async function updateStripeInfoDATABASE({user_id, customer_id, email}){
+    try{  
+  
+        console.log(`${invoice_id}, ${subscription_id}, ${client_id}`)
+  
+        const _mutation = gql`
+        mutation UpdateInfo(
+                $user_id: String!, 
+                $customer_id: String!, 
+                $email: String!
+            ){               
+            update_stripe_info(
+                where: {user_id: {_eq: $user_id}}, 
+                _set: {customer_id: $customer_id, email: $email}) {
+                affected_rows
+            }
+        }`; 
+
+        var data = await client.mutate({
+          mutation: _mutation,
+          variables:{
+            user_id: user_id,
+            customer_id: customer_id,
+            email: email
+  
+          }
+        })
+        if(data != null ){
+          console.log(`SUCCESS > ${data}`)
+        }
+    }
+    catch(e){
+      console.log(e)
+    }
+  }
--- pages/api/webhooks/customers/index.js
+++ pages/api/webhooks/customers/index.js
@@ -3,7 +3,7 @@ import { gql } from '@apollo/client';
 import { buffer } from 'micro';
 
 const stripe = require('stripe')( process.env.NEXT_STRIPE_API_SECRET, { apiVersion: "2023-10-16" } );
-const webhook_secret = process.env.WEBHOOK_ACCOUNTS_SECRET
+const webhook_secret = process.env.STRIPE_WEBHOOK_CUSTOMERS_SECRET
 
 export const config = {
   api: {
@@ -40,47 +40,25 @@ export default async function handler(req, res){
     mutation UpdateUser(
             $user_id: String!, 
             $customer_id: String!, 
+            $name: String!,
             $email: String!
         ){               
         update_stripe_info(
             where: {user_id: {_eq: $user_id}}, 
-            _set: {customer_id: $customer_id, email: $email}) {
+            _set: {customer_id: $customer_id, email: $email, name: $name}) {
             affected_rows
         }
     }`;
 
     switch (event.type) {
-        case 'customer.created':
-            const customerCreated = event.data.object;
-            
-    
-            var data = await client.mutate({
-                mutation: _mutation,
-                variables:{
-                    user_id: customerCreated.metadata.user_id,
-                    customer_id: customerCreated.id,
-                    email: customerCreated.email
-                }
-            })
-
-            if(data != null ){
-                console.log(data)
-                return res.status(200).json({
-                    accountUpdated: customerCreated.id,
-                    metadata: customerCreated.metadata,
-                    userId: customerCreated.metadata.user_id
-                })
-            }
-        break;
-
-
-        case 'customer.updated':
+         case 'customer.updated':
             const customerUpdated = event.data.object;
     
             var data = await client.mutate({
                 mutation: _mutation,
                 variables:{
                     user_id: customerUpdated.metadata.user_id,
+                    name: customerUpdated.name,
                     customer_id: customerUpdated.id,
                     email: customerUpdated.email
                 }
@@ -101,45 +79,6 @@ export default async function handler(req, res){
             console.log(`Unhandled event type ${event.type}`);
     }
 
-    if (event.type === "customer.created") { 
-
-        const accountUpdated = event.data.object;
-
-        const _mutation = gql`
-        mutation UpdateUser($user_id: String!, $role: user_role_enum!, $stripe_connect_id: String!, $email: String!) {
-          
-          update_user_by_pk(
-            pk_columns: {id: $user_id}, 
-            _set: {role: $role, stripe_connect_id: $stripe_connect_id}) {
-              id
-            }
-          
-            update_stripe_info(
-              where: {user_id: {_eq: $user_id}}, 
-              _set: {connect_id: $stripe_connect_id, email: $email}) {
-              affected_rows
-            }
-
-        }`;
-
-        var data = await client.mutate({
-          mutation: _mutation,
-          variables:{
-            user_id: accountUpdated.metadata.user_id,
-            role: "CREATOR",
-            stripe_connect_id: accountUpdated.id,
-            email: accountUpdated.email
-          }
-      })
-      if(data != null ){
-        // Return a 200 response to acknowledge receipt of the event
-        console.log(data)
-        return res.status(200).json({
-          accountUpdated: accountUpdated.id,
-          metadata: accountUpdated.metadata,
-          userId: accountUpdated.metadata.user_id
-        })
-      }}
 
   } catch (err) {
     console.log(err)
